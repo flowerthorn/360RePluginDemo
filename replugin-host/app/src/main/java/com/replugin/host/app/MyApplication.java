64224035e98fdc4bc07b7af55282237045185b7f
芒果 <mangguo@lingyongqian.cn>
2019-01-23 17:08:52 +0800

    feature:主程序接入配置

---
+++ replugin-host/app/src/main/java/com/replugin/host/app/MyApplication.java
@@ -0,0 +1,11 @@
+package com.replugin.host.app;
+
+
+import com.qihoo360.replugin.RePluginApplication;
+
+/**
+ * Created by lihongxin on 2019/1/23
+ */
+public class MyApplication extends RePluginApplication {
+
+}
